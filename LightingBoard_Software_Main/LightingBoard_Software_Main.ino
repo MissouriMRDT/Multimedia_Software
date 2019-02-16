4ad9b324bfd1599e240956403a11815eda4ed547
liberalty <liberalty@users.noreply.github.com>
2019-02-16 14:07:43 -0600

    PWM pins trouble
    Using PD1 and PD0 as PWM pins, but only the PD1 pins works with different intesities while PD0 only works with full intensity (255)

--- LightingBoard_Software_Main/LightingBoard_Software_Main.ino
+++ LightingBoard_Software_Main/LightingBoard_Software_Main.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
-stuff
+// Lighting Board Software//////////////////////////////////////////////////////////////
+    // Main Header file
+//
+// Created for 2019 Valkyrie by John Beuke, jrbyh6@mst.edu
+////////////////////////////////////////////////////////////////////////////////////////
+// This is the new and better code than the original jank code created by John Beuke
+// This will intergrate headlight control with neopixels control to create "lighting board'
+////2-15-2018////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include "LightingBoard_Software_Main.h"
+
+
+void setup()
+{
+  Serial.begin(9600);
+  //RoveComm.begin(RC_LIGHTINGSHIMBLEBOARD_FOURTHOCTET); //sets the fourth octet of the ip address for networking with the Rover network
+  //delay  (ROVECOMM_DELAY);                      // sets 10ms delay so code doesnt trip over itself
+
+  pinMode(HEADLIGHT1_PIN, OUTPUT);
+  //pinMode(HEADLIGHT2_PIN, OUTPUT);
+  Serial.println("Setup Complete.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly: 
+  
+void loop() 
+{
+
+  delay(100);
+  analogWrite(HEADLIGHT1_PIN, 0);
+  analogWrite(HEADLIGHT2_PIN, 100);
   
 }
