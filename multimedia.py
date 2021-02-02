4e61e87e07e194ad79b2a32bf32a18836d97862f
Eli Verbrugge <eli.verbrugge@gmail.com>
2021-02-01 18:54:29 -0600

    Changing data ids, to align with 7000 instead of 7001

--- multimedia.py
+++ multimedia.py
@@ -11,11 +11,10 @@ if __name__ == "__main__":
     lighting.clear_lights()
 
     # Set up the callbacks
+    rovecomm_node.set_callback(7001, lighting.handle_lighting_commands)
     rovecomm_node.set_callback(7002, lighting.handle_lighting_commands)
     rovecomm_node.set_callback(7003, lighting.handle_lighting_commands)
-    rovecomm_node.set_callback(7004, lighting.handle_lighting_commands)
-        
+
     while True:
         # Keep light panel running
         lighting.run_lighting()
-    
